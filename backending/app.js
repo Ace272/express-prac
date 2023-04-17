796647b3b0a3d0a0e305b08d8f0825f5ba746bd2
Ace272 <ace272@icloud.com>
2023-04-17 22:51:51 +0300

    redirected the routing to the ejs files

--- backending/app.js
+++ backending/app.js
@@ -11,20 +11,26 @@ app.listen(3000);
 
 app.get('/', (req,res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html',{root: __dirname})
+    //res.sendFile('./views/index.ejs',{root: __dirname})
+    res.render('index');
 });
 
 app.get('/about',(req,res) => {
     // res.send('The about page');
-    res.sendFile('./views/about.html', {root: __dirname});
+    //res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about')
 });
 
 //redirects
-app.get('/about-us', (req,res) => {
-    res.redirect('/about');
+// app.get('/about-us', (req,res) => {
+//     res.redirect('/about');
+// });
+
+app.get('/blogs/create', (req,res) => {
+    res.render('create');
 });
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
-})
+    res.status(404).render('404');
+});
