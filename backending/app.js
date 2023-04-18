fb2cbdea41402fb50376e340d7e2e7b9f888a444
Ace272 <ace272@icloud.com>
2023-04-18 23:51:46 +0300

    exploring middleware

--- backending/app.js
+++ backending/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const morgan = require('morgan');
+//exploring more on middleware
 // express app
 const app = express();
 
@@ -8,6 +9,10 @@ app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000);
+
+//middleware and static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req,res) => {
     // res.send('<p>home page</p>');
